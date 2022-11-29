04a10bb47eca545d9ef47105089b6d8c47e33a58
MiguelChaves <mcharod03@gmail.com>
2022-11-29 19:36:58 +0100

    Subido

---
+++ UT2/Actividades/Actividad_4/src/main/java/Actividad_4/Actividad4.java
@@ -0,0 +1,62 @@
+package Actividad_4;
+
+import java.util.*;
+
+public class Actividad4 {
+
+	public static void main (String [] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		
+		MyHilo hilo = new MyHilo ();
+
+		String cad;
+		
+		while (true) {
+			
+			System.out.print ("\tIntroduce CADENA: ");
+			cad = sc.nextLine ();
+			
+			if (!hilo.isAlive ()) {
+				
+				System.out.println ("");
+				
+				System.out.println ("LANZANDO HILO ...");
+				
+				System.out.println ("");
+				
+				hilo.start ();
+				
+			}
+
+			if (cad.equals ("*")) {
+				
+				break;
+			
+			}
+			
+			if (cad.equals ("S")) {
+				
+				hilo.Suspende ();
+			
+			}
+			
+			if (cad.equals ("R")) {
+				
+				hilo.Reanuda ();
+			
+			}
+			
+		}		
+		
+		hilo.Reanuda ();
+		
+		System.out.println ("");
+		
+		System.out.println ("CONTADOR:" + hilo.getContador ());
+		
+		hilo.pararHilo ();
+		
+	}
+
+}
---
+++ UT2/Actividades/Actividad_4/src/main/java/Actividad_4/SolicitaSuspender.java
@@ -0,0 +1,25 @@
+package Actividad_4;
+
+public class SolicitaSuspender {
+
+	private boolean solicitaSuspender;
+
+	public synchronized void set (boolean b) {
+		
+		solicitaSuspender = b;
+		
+		notifyAll ();
+		
+	}
+
+	public synchronized void esperandoParaReanudar () throws InterruptedException {
+		
+		while (solicitaSuspender) {
+			
+			wait ();
+			
+		}
+		
+	}
+	
+}
